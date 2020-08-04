b565ae13d007c46ddc41b6e3edf042d4b7230788
SMART-TTS <60169197+SMART-TTS@users.noreply.github.com>
2020-08-04 13:35:20 +0900

    Update train_transformer.py
    update file to split train_transformer / train_stoptoken

--- train_transformer.py
+++ train_transformer.py
@@ -1,6 +1,5 @@
 from preprocess import get_dataset, DataLoader, collate_fn_transformer
 from network import *
-from module import GuidedLoss 
 from tensorboardX import SummaryWriter
 import torchvision.utils as vutils
 import os
@@ -10,17 +9,19 @@ import sys
 from util.writer import get_writer
 import time
 #import torch
-
-
 def adjust_learning_rate(optimizer, step_num, warmup_step=4000):
     lr = hp.lr * warmup_step**0.5 * min(step_num * warmup_step**-1.5, step_num**-0.5)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-
 def stream(message):
     sys.stdout.write(f"\n{message}")
 
+def get_mask_from_lengths(lengths):
+    max_len = t.max(lengths).item()
+    ids = lengths.new_tensor(t.arange(0, max_len))
+    mask = (lengths.unsqueeze(1) > ids).to(t.bool)
+    return mask
 
 def validate(m, val_loader, global_step, writer):
     m.eval()
@@ -28,26 +29,41 @@ def validate(m, val_loader, global_step, writer):
         n_data, val_loss = 0, 0
         for i, data in enumerate(val_loader):
             n_data += len(data[0])
-            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, emo_code = data
-            stop_tokens = t.abs(pos_mel.ne(0).type(t.float) - 1).cuda()
-            
+            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, fname = data
+           
             character = character.cuda()
             mel = mel.cuda()
             mel_input = mel_input.cuda()
             pos_text = pos_text.cuda()
             pos_mel = pos_mel.cuda()
             
-            mel_pred, postnet_pred, attn_probs, stop_preds, attns_enc, attns_dec = m.forward(character, mel_input, pos_text, pos_mel, emo_code)
+            mel_pred, postnet_pred, attn_probs, decoder_outputs, attns_enc, attns_dec = m.forward(character, mel_input, pos_text, pos_mel)
 
             mel_loss = nn.L1Loss()(mel_pred, mel)
             post_mel_loss = nn.L1Loss()(postnet_pred, mel)
-            # stop_token_loss = nn.BCEWithLogitsLoss(pos_weight=t.tensor(hp.bce_pos_weight))(stop_preds, stop_tokens)
-            # guide_loss = guided_loss(attn_probs, text_length, mel_length, global_step) # isn't it correct to write guide?
 
-            # loss = mel_loss + post_mel_loss + guide_loss
+            mask = get_mask_from_lengths(mel_length).cuda()
+   
             loss = mel_loss + post_mel_loss
             val_loss += loss.item()
         val_loss /= n_data
+    attns_enc_new=[]
+    attns_dec_new=[]
+    attn_probs_new=[]
+    for i in range(len(attns_enc)):
+        attns_enc_new.append(attns_enc[i].unsqueeze(0))
+        attns_dec_new.append(attns_dec[i].unsqueeze(0))
+        attn_probs_new.append(attn_probs[i].unsqueeze(0))
+    attns_enc = t.cat(attns_enc_new, 0)
+    attns_dec = t.cat(attns_dec_new, 0)
+    attn_probs = t.cat(attn_probs_new, 0)
+
+    attns_enc = attns_enc.contiguous().view(attns_enc.size(0), hp.batch_size, hp.n_heads, attns_enc.size(2), attns_enc.size(3))
+    attns_enc = attns_enc.permute(1,0,2,3,4)
+    attns_dec = attns_dec.contiguous().view(attns_dec.size(0), hp.batch_size, hp.n_heads, attns_dec.size(2), attns_dec.size(3))
+    attns_dec = attns_dec.permute(1,0,2,3,4)
+    attn_probs = attn_probs.contiguous().view(attn_probs.size(0), hp.batch_size, hp.n_heads, attn_probs.size(2), attn_probs.size(3))
+    attn_probs = attn_probs.permute(1,0,2,3,4)
     save_dir = os.path.join(hp.checkpoint_path, hp.log_directory, 'figure')
     writer.add_losses(mel_loss.item(), post_mel_loss.item(), global_step, 'Validation')
     writer.add_alignments(attns_enc.detach().cpu(), attns_dec.detach().cpu(), attn_probs.detach().cpu(), mel_length, text_length, global_step, 'Validation', save_dir)
@@ -69,8 +85,8 @@ def main():
         restore_flag = False
 
     m = Model()
-    if os.path.exists('./checkpoints_guide/checkpoint_%s_%d.pth.tar'% ('transformer', global_step)):
-        state_dict = t.load('./checkpoints_guide/checkpoint_%s_%d.pth.tar'% ('transformer', global_step))
+    if os.path.exists('./checkpoints/checkpoint_%s_%d.pth.tar'% ('transformer', global_step)):
+        state_dict = t.load('./checkpoints/checkpoint_%s_%d.pth.tar'% ('transformer', global_step))   
         new_state_dict = OrderedDict()
         for k, value in state_dict['model'].items():
             key = k[7:]
@@ -82,26 +98,23 @@ def main():
     m.train()
     optimizer = t.optim.Adam(m.parameters(), lr=hp.lr)
 
+
     pos_weight = t.FloatTensor([5.]).cuda()
     writer = get_writer(hp.checkpoint_path, hp.log_directory)
     cur_epoch = 0
 
     for epochs in range(hp.epochs):
         train_dataloader = DataLoader(train_dataset, batch_size=hp.batch_size, shuffle=True, collate_fn=collate_fn_transformer, drop_last=True, num_workers=1)
-        val_dataloader = DataLoader(val_dataset, batch_size=hp.batch_size, shuffle=True, collate_fn=collate_fn_transformer, drop_last=True)
+        val_dataloader = DataLoader(val_dataset, batch_size=hp.batch_size, shuffle=True, collate_fn=collate_fn_transformer, drop_last = True)
         if restore_flag:
             cur_epoch = int(restore_step/len(train_dataloader))
             restore_flag = not restore_flag
-        # pbar = tqdm(train_dataloader)
         for i, data in enumerate(train_dataloader):
-            # start = time.time()
-            # pbar.set_description("Processing at epoch %d"%cur_epoch)
             global_step += 1
             if global_step < 400000:
                 adjust_learning_rate(optimizer, global_step)
-
-            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, emo_code = data
-            stop_tokens = t.abs(pos_mel.ne(0).type(t.float) - 1).cuda()	# [0,0,...,0,0,1,1,1,...,1,1]
+                
+            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, fname =  data
             character = character.cuda()
             mel = mel.cuda()
             mel_input = mel_input.cuda()
@@ -110,18 +123,11 @@ def main():
             text_length = text_length.cuda()
             mel_length = mel_length.cuda()
             loading_time = time.time()
-            mel_pred, postnet_pred, attn_probs, stop_preds, attns_enc, attns_dec = m.forward(character, mel_input, pos_text, pos_mel, emo_code)
-
+            mel_pred, postnet_pred, attn_probs, decoder_output, attns_enc, attns_dec = m.forward(character, mel_input, pos_text, pos_mel)
             mel_loss = nn.L1Loss()(mel_pred, mel)
             post_mel_loss = nn.L1Loss()(postnet_pred, mel)
-            # stop_token_loss = nn.BCEWithLogitsLoss(pos_weight=t.tensor(hp.bce_pos_weight))(stop_preds, stop_tokens)
-            guide = GuidedLoss()
-            guide_loss = guide(attn_probs, text_length, mel_length, global_step)
 
-            # if epochs % 10 and epochs != 0:
-            #     loss = (mel_loss + post_mel_loss + 0.1*stop_token_loss + guide_loss)/hp.accum
-            # else:
-            loss = (mel_loss + post_mel_loss + guide_loss)/hp.accum
+            loss = (mel_loss + post_mel_loss)/hp.accum
             writer.add_losses(mel_loss.item(), post_mel_loss.item(), global_step, 'Train')
 
             # Calculate gradients
@@ -139,10 +145,15 @@ def main():
                 validate(m, val_dataloader, global_step, writer)
 
             if global_step % hp.save_step == 0:
-                t.save({'model':m.state_dict(), 'optimizer':optimizer.state_dict()},
-                       os.path.join(hp.checkpoint_path,'checkpoint_transformer_%d.pth.tar' % global_step))
+                t.save({'model':m.state_dict(),
+                                 'optimizer':optimizer.state_dict()},
+                                os.path.join(hp.checkpoint_path,'checkpoint_transformer_%d.pth.tar' % global_step))
+        if cur_epoch == hp.stop_epoch:
+            break
         cur_epoch += 1
         print(' ')
+            
+            
 
 
 if __name__ == '__main__':
