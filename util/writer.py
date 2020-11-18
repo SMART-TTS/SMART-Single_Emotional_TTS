26ef720eaa33d1723a6bf5cc379c249d4f19deb4
Byoung Jin Choi <bjchoi@hi.snu.ac.kr>
2020-11-18 23:03:45 +0900

    writer update

--- util/writer.py
+++ util/writer.py
@@ -21,9 +21,21 @@ class TTSWriter(SummaryWriter):
     def __init__(self, log_dir):
         super(TTSWriter, self).__init__(log_dir)
         
-    def add_losses(self, mel_loss, post_mel_loss, global_step, phase):
+    def add_losses(self, mel_loss, post_mel_loss, stop_token_loss, post_linear_loss, global_step, phase):
         self.add_scalar(f'{phase}_mel_loss', mel_loss, global_step)
         self.add_scalar(f'{phase}_post_mel_loss', post_mel_loss, global_step)
+        self.add_scalar(f'{phase}_stop_token_loss', stop_token_loss, global_step)
+        self.add_scalar(f'{phase}_post_linear_loss', post_linear_loss, global_step)
+     
+    def add_specs(self, mel_padded, mel_out, mel_out_post, mel_lengths, global_step, phase):
+        mel_fig = plot_melspec(mel_padded, mel_out, mel_out_post, mel_lengths)
+        self.add_figure(f'{phase}_melspec', mel_fig, global_step)
+        if not os.path.exists(save_fig_dir):
+            os.makedirs(save_fig_dir)
+        mel_fig_dir = os.path.join(save_fig_dir, 'mel')
+        if not os.path.exists(mel_fig_dir):
+            os.makedirs(mel_fig_dir)
+        mel_fig.savefig(f'{mel_fig_dir}/{global_step}')
         
     def add_alignments(self, enc_alignments, dec_alignments, enc_dec_alignments, style_alignments,
                        mel_lengths, text_lengths, global_step, phase, save_fig_dir):
@@ -55,3 +67,11 @@ class TTSWriter(SummaryWriter):
             os.makedirs(style_fig_dir)
         style_align_fig.savefig(f'{style_fig_dir}/{global_step}')
 
+
+    def add_gates(self, gate_out, global_step, phase):
+        gate_fig = plot_gate(gate_out)
+        self.add_figure(f'{phase}_gate_out', gate_fig, global_step)
+        gate_fig_dir = os.path.join(save_fig_dir, 'gate')
+        if not os.path.exists(gate_fig_dir):
+            os.makedirs(gate_fig_dir)
+        gate_fig.savefig(f'{gate_fig_dir}/{global_step}')       
