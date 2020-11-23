3e604a68baac2546b96fd3e693d3736de4ae24ea
Byoung Jin Choi <bjchoi@hi.snu.ac.kr>
2020-11-23 18:17:50 +0900

    fixing train_transformer for STFT loss

--- train_transformer.py
+++ train_transformer.py
@@ -9,6 +9,7 @@ from utils import spectrogram2wav
 from scipy.io.wavfile import write
 from util.writer import get_writer
 import time
+#import torch
 from mel2audio.args import parse_args
 from mel2audio.hps import Hyperparameters
 from mel2audio.model import SmartVocoder
@@ -29,29 +30,39 @@ def get_mask_from_lengths(lengths):
 
 def validate(m, vocoder, val_loader, global_step, writer):
     m.eval()
+ 
     with t.no_grad():
         n_data, val_loss = 0, 0
         for i, data in enumerate(val_loader):
             n_data += len(data[0])
-            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, fname = data
-           
+            character, mel, mag, mel_input, pos_text, pos_mel, text_length, mel_length, fname = data
+
+            mel_max_length_array = t.zeros(mel_length.size(0)).long()
+            mel_max_length_array = t.LongTensor(mel_max_length_array)
+            mel_max_length_array[:] = t.max(mel_length)
+            mel_max_length_array = mel_max_length_array.cuda()
+
             character = character.cuda()
             mel = mel.cuda()
+            mag = mag.cuda()
             mel_input = mel_input.cuda()
             pos_text = pos_text.cuda()
             pos_mel = pos_mel.cuda()
-            
-            mel_pred, postnet_pred, attn_probs, decoder_outputs, attns_enc, attns_dec, attns_style = m.forward(character, mel_input, pos_text, pos_mel, mel, pos_mel)
-
-            mel_loss = nn.L1Loss()(mel_pred, mel)
-            post_mel_loss = nn.L1Loss()(postnet_pred, mel)
-
+            text_length = text_length.cuda()
+            mel_length = mel_length.cuda()
             mask = get_mask_from_lengths(mel_length).cuda()
-   
-            loss = mel_loss + post_mel_loss
+
+            mel_pred, postnet_pred, attn_probs, decoder_outputs, attns_enc, attns_dec, attns_style, post_linear, duration_predictor_output, duration, weights = m.forward(character, mel_input, pos_text, pos_mel, mel, pos_mel, mel_max_length_array=mel_max_length_array)
+
+            mel_loss = t.mean(t.abs(mel_pred-mel).masked_select(mask.unsqueeze(-1)))
+            post_mel_loss = t.mean(t.abs(postnet_pred-mel).masked_select(mask.unsqueeze(-1)))
+            n_priority_freq = int(2000 / (hp.sr*0.5) * (hp.n_fft/2+1))
+            post_linear_loss = 0.5*t.mean(t.abs(post_linear-mag).masked_select(mask.unsqueeze(-1))) + 0.5*t.mean(t.abs(post_linear-mag)[:, :, :n_priority_freq].masked_select(mask.unsqueeze(-1)))
+            duration_loss = nn.L1Loss()(t.sum(duration_predictor_output, -1, keepdim=True), mel_length) / t.sum(text_length)
+
+            loss = mel_loss + post_mel_loss + 0.3*post_linear_loss + duration_loss
             val_loss += loss.item()
         val_loss /= n_data
-    mag_pred = m_post.forward(postnet_pred)
 
     for i, mel_pred in enumerate(postnet_pred[:3]):
         mel_v = mel_pred.unsqueeze(0).transpose(2,1)
@@ -69,6 +80,27 @@ def validate(m, vocoder, val_loader, global_step, writer):
         np.save(os.path.join(mel_path, "val_{}_synth_{}.mel".format(fname[i], global_step)), mel_pred.unsqueeze(0).cpu())
 
         wav_path = os.path.join(os.path.join(hp.checkpoint_path, hp.log_directory), 'wav')
+        if not os.path.exists(wav_path):
+            os.makedirs(wav_path)
+        write(os.path.join(wav_path, "val_{}_synth_{}.wav".format(fname[i], global_step)), hp.sr, wav)
+        print("written as val_{}_synth.wav".format(fname[i]))
+
+    for i, mel_ in enumerate(mel[:3]):
+        mel_v = mel_.unsqueeze(0).transpose(2,1)
+        mel_v = (mel_v*100+20-100)/20
+        B, C, T = mel_v.shape
+        z = t.randn(1, 1, T*hp.hop_length).cuda()
+        z = z * 0.6     # Temp
+        with t.no_grad():
+            y_gen = vocoder.reverse(z, mel_v).squeeze()
+            wav = y_gen.to(t.device("cpu")).data.numpy()
+
+        mel_path = os.path.join(os.path.join(hp.checkpoint_path, hp.log_directory), 'mel_recon')
+        if not os.path.exists(mel_path):
+            os.makedirs(mel_path)
+        np.save(os.path.join(mel_path, "val_{}_synth_{}.mel".format(fname[i], global_step)), mel_.unsqueeze(0).cpu())
+
+        wav_path = os.path.join(os.path.join(hp.checkpoint_path, hp.log_directory), 'wav_recon')
         if not os.path.exists(wav_path):
             os.makedirs(wav_path)
         write(os.path.join(wav_path, "val_{}_synth_{}.wav".format(fname[i], global_step)), hp.sr, wav)
@@ -98,10 +130,10 @@ def validate(m, vocoder, val_loader, global_step, writer):
     attns_style = attns_style.permute(1,0,2,3,4)
 
     save_dir = os.path.join(hp.checkpoint_path, hp.log_directory, 'figure')
-    writer.add_losses(mel_loss.item(), post_mel_loss.item(), global_step, 'Validation')
+    writer.add_losses(mel_loss.item(), post_mel_loss.item(), 0.3*post_linear_loss.item(), duration_loss.item(), global_step, 'Validation')
     writer.add_alignments(attns_enc.detach().cpu(), attns_dec.detach().cpu(), attn_probs.detach().cpu(), attns_style.detach().cpu(), mel_length, text_length, global_step, 'Validation', save_dir)
 
-    msg = "Validation| loss : {:.4f} + {:.4f} = {:.4f}".format(mel_loss, post_mel_loss, loss)
+    msg = "Validation| loss : {:.4f} + {:.4f} + {:.4f} + {:.4f} = {:.4f}".format(mel_loss, post_mel_loss, 0.3*post_linear_loss, duration_loss, loss)
     stream(msg)
     m.train()
 
@@ -134,7 +166,7 @@ def main():
     vocoder.load_state_dict(t.load('./mel2audio/checkpoint_step000588458.pth')["state_dict"])
     vocoder=vocoder.cuda()
     vocoder.eval()
-	
+
     optimizer = t.optim.Adam(m.parameters(), lr=hp.lr)
 
     writer = get_writer(hp.checkpoint_path, hp.log_directory)
@@ -151,26 +183,38 @@ def main():
             if global_step < 400000:
                 adjust_learning_rate(optimizer, global_step)
                 
-            character, mel, mel_input, pos_text, pos_mel, text_length, mel_length, fname =  data
+            character, mel, mag, mel_input, pos_text, pos_mel, text_length, mel_length, fname =  data
+
+            mel_max_length_array = t.zeros(mel_length.size(0)).long()
+            mel_max_length_array = t.LongTensor(mel_max_length_array)
+            mel_max_length_array[:] = t.max(mel_length)
+            mel_max_length_array = mel_max_length_array.cuda()
 
             character = character.cuda()
             mel = mel.cuda()
+            mag = mag.cuda()
             mel_input = mel_input.cuda()
             pos_text = pos_text.cuda()
             pos_mel = pos_mel.cuda()
             text_length = text_length.cuda()
             mel_length = mel_length.cuda()
             loading_time = time.time()
-            mel_pred, postnet_pred, attn_probs, decoder_output, attns_enc, attns_dec, attns_style = m.forward(character, mel_input, pos_text, pos_mel, mel, pos_mel)
-            mel_loss = nn.L1Loss()(mel_pred, mel)
-            post_mel_loss = nn.L1Loss()(postnet_pred, mel)
+            mask = get_mask_from_lengths(mel_length).cuda()
 
-            loss = (mel_loss + post_mel_loss)/hp.accum
-            writer.add_losses(mel_loss.item(), post_mel_loss.item(), global_step, 'Train')
+            mel_pred, postnet_pred, attn_probs, decoder_outputs, attns_enc, attns_dec, attns_style, post_linear, duration_predictor_output, duration, weights = m.forward(character, mel_input, pos_text, pos_mel, mel, pos_mel, mel_max_length_array=mel_max_length_array)
+
+            mel_loss = t.mean(t.abs(mel_pred-mel).masked_select(mask.unsqueeze(-1)))
+            post_mel_loss = t.mean(t.abs(postnet_pred- mel).masked_select(mask.unsqueeze(-1)))
+            n_priority_freq = int(2000 / (hp.sr*0.5) * (hp.n_fft/2+1))
+            post_linear_loss = 0.5*t.mean(t.abs(post_linear-mag).masked_select(mask.unsqueeze(-1))) + 0.5*t.mean(t.abs(post_linear-mag)[:, :, :n_priority_freq].masked_select(mask.unsqueeze(-1)))
+            duration_loss = nn.L1Loss()(t.sum(duration_predictor_output, -1, keepdim=True), mel_length) / t.sum(text_length)
+
+            loss = (mel_loss + post_mel_loss + 0.3*post_linear_loss + duration_loss)/hp.accum
+            writer.add_losses(mel_loss.item(), post_mel_loss.item(), 0.3*post_linear_loss, duration_loss, global_step, 'Train')
 
             # Calculate gradients
             loss.backward()
-            msg = "| Epoch: {}, {}/{}th loss : {:.4f} + {:.4f} = {:.4f}".format(cur_epoch, i, len(train_dataloader), mel_loss, post_mel_loss, loss)
+            msg = "| Epoch: {}, {}/{}th loss : {:.4f} + {:.4f} + {:.4f} + {:.4f} = {:.4f}".format(cur_epoch, i, len(train_dataloader), mel_loss, post_mel_loss, 0.3*post_linear_loss, duration_loss, loss)
             stream(msg)
 
             if global_step % hp.accum == 0:
